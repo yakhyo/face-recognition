1bfe5f820d2fdabbb9ad0f1beea8c726efbeee30
yakhyo <yakhyo9696@gmail.com>
2024-11-28 07:45:48 +0000

    ref: Update several more files

---
+++ scripts/readme.txt
@@ -0,0 +1,3 @@
+These datasets are associated with the OpenSphere project and can be found at the following GitHub repository: OpenSphere Scripts. The repository provides scripts and resources for face recognition and related tasks, including datasets, pre-processing tools, and training configurations.
+
+https://github.com/ydwen/opensphere
--- train.py
+++ train.py
@@ -1,48 +1,50 @@
-from utils import layer
-import lfw_eval
-from utils.dataset import ImageList
-from models import net, mobilefacenet
+
 import argparse
 import os
 import time
 import torch
 
 import torch.utils.data
-from torchvision import datasets, transforms
-import torch.backends.cudnn as cudnn
+from torchvision import transforms
 
-cudnn.benchmark = True
+from utils import layer
+import lfw_eval
+from utils.dataset import ImageList, ImageFolder
+
+from utils.helper import AverageMeter, calculate_accuracy
+
+from models import mobilefacenet
+from models.sphereface import sphere20, sphere36, sphere64
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch CosFace')
-parser.add_argument('--root_path', type=str, default='', help='path to root path of images')
+parser.add_argument('--root_path', type=str, default='data/train/webface_112x112', help='path to root path of images')
 parser.add_argument('--database', type=str, default='WebFace', help='Which Database for train. (WebFace, VggFace2)')
 parser.add_argument('--train_list', type=str, default=None, help='path to training list')
 parser.add_argument('--batch_size', type=int, default=256, help='input batch size for training (default: 512)')
 parser.add_argument('--is_gray', action='store_true', help='Transform input image to gray')
-parser.add_argument('--network', type=str, default='mobilenet', help='Which network for train.')
-# parser.add_argument('--network', type=str, default='mobilenet', help='Which network for train.')
+parser.add_argument('--network', type=str, default='sphere20', help='Which network for train.')
 parser.add_argument('--num_class', type=int, default=None, help='number of people (class)')
-parser.add_argument('--classifier_type', type=str, default='ARC', help='Which classifier for train.')
+parser.add_argument('--classifier_type', type=str, default='MCP', help='Which classifier for train.')
 parser.add_argument('--epochs', type=int, default=30, help='number of epochs to train (default: 30)')
 parser.add_argument('--lr', type=float, default=0.1, help='learning rate (default: 0.1)')
 parser.add_argument('--step_size', type=list, default=None, help='lr decay step')
 parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum (default: 0.9)')
 parser.add_argument('--weight_decay', type=float, default=5e-4, help='weight decay (default: 0.0005)')
-parser.add_argument('--log_interval', type=int, default=100, help='how many batches to wait before logging training status')
+parser.add_argument('--print-freq', type=int, default=100,
+                    help='how many batches to wait before logging training status')
 parser.add_argument('--save_path', type=str, default='checkpoint/', help='path to save checkpoint')
-parser.add_argument('--no_cuda', action='store_true', help='disables CUDA training')
 parser.add_argument('--workers', type=int, default=8, help='how many workers to load data')
 args = parser.parse_args()
 
-device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if args.database == 'WebFace':
     args.train_list = 'data/train/webface_train_ann.txt'
     args.num_class = 10572
     args.step_size = [10, 20]
 elif args.database == 'VggFace2':
-    args.train_list = '/home/wangyf/dataset/VGG-Face2/VGG-Face2-112X96.txt'
+    args.train_list = '/data/train/vggface2_train_ann.txt'
     args.num_class = 8069
     args.step_size = [80000, 120000, 140000]
 else:
@@ -51,16 +53,21 @@ else:
 
 def main():
     # Model selection based on arguments
+    in_channels = 1 if args.is_gray else 3
     if args.network == 'sphere20':
-        model = net.SphereNet(type=20, is_gray=args.is_gray)
+        model = sphere20(embedding_dim=512, in_channels=in_channels)
+        model_eval = sphere20(embedding_dim=512, in_channels=in_channels)
+    elif args.network == 'sphere36':
+        model = sphere36(embedding_dim=512, in_channels=in_channels)
+        model_eval = sphere36(embedding_dim=512, in_channels=in_channels)
     elif args.network == 'sphere64':
-        model = net.SphereNet(type=64, is_gray=args.is_gray)
-    elif args.network == 'LResNet50E_IR':
-        model = net.LResNet50E_IR(is_gray=args.is_gray)
-    elif args.network=="mobilenet":
-        model = mobilefacenet.get_mbf(False, 512)
+        model = sphere64(embedding_dim=512, in_channels=in_channels)
+        model_eval = sphere64(embedding_dim=512, in_channels=in_channels)
+    elif args.network == "mobile":
+        model = MobileNetV2(input_size=(112, 112))
+        model_eval = MobileNetV2(input_size=(112, 112))
     else:
-        raise ValueError("NOT SUPPORT NETWORK!")
+        raise ValueError("NOT SUPPORT NETWORK! ")
 
     # No need for DataParallel, we are using a single GPU
     model = model.to(device)
@@ -94,11 +101,10 @@ def main():
 
     # DataLoader
     train_loader = torch.utils.data.DataLoader(
-        ImageList(root=args.root_path, fileList=args.train_list, transform=train_transform),
+        ImageFolder(root=args.root_path, transform=train_transform),
         batch_size=args.batch_size, shuffle=True,
         num_workers=args.workers, pin_memory=True, drop_last=True
     )
-    
 
     print(f'Length of train dataset: {len(train_loader.dataset)}, Number of Identities: {args.num_class}')
 
@@ -116,30 +122,76 @@ def main():
 
     # Training loop
     for epoch in range(1, args.epochs + 1):
-        train(train_loader, model, classifier, criterion, optimizer, epoch)
+        train_one_epoch(model, classifier, criterion, optimizer, train_loader, device, epoch)
         save_path = os.path.join(args.save_path, f'{args.network}_{epoch}_checkpoint.pth')
         torch.save(model.state_dict(), save_path)
         scheduler.step()
+        lfw_eval.eval(model_eval, save_path, args.is_gray)
 
 
-def train(train_loader, model, classifier, criterion, optimizer, epoch):
+def validate():
+    pass
+
+
+def train_one_epoch(model, classifier, criterion, optimizer, data_loader, device, epoch) -> None:
     model.train()
+
+    losses = AverageMeter("Avg Loss", ":6.3f")
+    batch_time = AverageMeter("Batch Time", ":4.3f")
+    accuracy_meter = AverageMeter("Accuracy", ":4.2f")
+
     start_time = time.time()
+    for batch_idx, (images, target) in enumerate(data_loader):
+        # Move data to device
+        images = images.to(device)
+        target = target.to(device)
 
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        output = model(data)
-        output = classifier(output, target) if not isinstance(classifier, torch.nn.Linear) else classifier(output)
-        loss = criterion(output, target)
-
+        # Reset gradients
         optimizer.zero_grad()
+
+        # Forward pass
+        embeddings = model(images)
+        if isinstance(classifier, torch.nn.Linear):
+            output = classifier(embeddings)
+        else:
+            output = classifier(embeddings, target)
+
+        # Compute loss and accuracy
+        loss = criterion(output, target)
+        accuracy = calculate_accuracy(output, target)
+        # print(accuracy)
+
+        # Backward pass
         loss.backward()
+
+        # Update model parameters
         optimizer.step()
 
-        if batch_idx % args.log_interval == 0:
-            elapsed_time = time.time() - start_time
-            print(f'Epoch: {epoch}, Batch: {batch_idx}/{len(train_loader)}, Loss: {loss.item():.6f}, Time: {elapsed_time:.4f}s')
-            start_time = time.time()
+        # Update metrics
+        losses.update(loss.item(), images.size(0))
+        accuracy_meter.update(accuracy.item(), images.size(0))
+        batch_time.update(time.time() - start_time)
+
+        # Reset start time for the next batch
+        start_time = time.time()
+
+        # Log results at intervals
+        if batch_idx % args.print_freq == 0:
+            print(
+                f'Epoch: [{epoch}][{batch_idx:05d}/{len(data_loader)}] '
+                f'Loss: {losses.avg:6.3f}, '
+                f'Accuracy: {accuracy_meter.avg:4.2f}%, '
+                f'Time: {batch_time.avg:4.3f}s'
+            )
+
+    # End-of-epoch summary
+    print(
+        f'Epoch [{epoch}] Summary: '
+        f'Loss: {losses.avg:6.3f}, '
+        f'Accuracy: {accuracy_meter.avg:4.2f}%, '
+        f'Total Time: {batch_time.sum:4.3f}s'
+    )
+    
 
 
 if __name__ == '__main__':
